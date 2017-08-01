c78a8637dee6cbd5e133b023511da8043351ba57
Daeyeol Ryu <dy@yoobato.com>
2017-08-01 23:45:31 +0800

    Parser 모듈로 분리

--- navergoodnewsparser.js
+++ navergoodnewsparser.js
@@ -1,49 +1,19 @@
 'use strict';
 
-var request = require('request'),
-    Iconv   = require('iconv').Iconv,
-    cheerio = require("cheerio"),
+var parser  = require('./lib/parser'),
     log     = require('./lib/log');
 
 log.info('*** 네이버 따뜻한 세상 뉴스 파서 시작 ***');
 
-var iconv = new Iconv('euc-kr', 'utf-8//translit//ignore');
-
 var naverHost = 'news.naver.com';
+var parser = new parser(naverHost);
 
 var currentPage = 1;
-var getNewsListOption = {
-    url: 'http://' + naverHost + '/main/hotissue/sectionList.nhn?sid1=102&cid=3069&page=' + currentPage,
-    encoding: null
-};
-request.get(getNewsListOption, function(error, response, html) {
-    if (error) {
-        throw error;
-    }
-
-    var htmlData = iconv.convert(html).toString();
-    var $ = cheerio.load(htmlData);
-    
-    var articleATags = $('dl.cnt dt.t a');
-    articleATags.each(function() {
-        var articleUrl = $(this).attr('href');
-
-        var getArticleOption = {
-            url: 'http://' + naverHost + articleUrl,
-            encoding: null
-        };
-        request.get(getArticleOption, function(error2, response2, html2) {
-            if (error2) {
-                throw error;
-            }
-
-            var htmlData2 = iconv.convert(html2).toString();
-            var $2 = cheerio.load(htmlData2);
-
-            var articleTitle = $2('#articleTitle').text();
-            console.log(articleTitle);
+parser.parseNaverArticlePathList(currentPage, function(articlePaths) {
+    for (var i = 0; i < articlePaths.length; i++) {
+        var articlePath = articlePaths[i];
+        parser.parseNaverArticleMetadata(articlePath, function(articleOriginalUrl, articleDate, articleImageDesc) {
+            console.log("URL: " + articleOriginalUrl + " | Date: " + articleDate + " | Desc: " + articleImageDesc);
         });
-
-        // console.log(url);
-    });
+    }
 });
