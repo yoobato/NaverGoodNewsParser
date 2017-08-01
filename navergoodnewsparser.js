ce8bed1a98d8a5eea063331b4836ee685f42f8c6
Daeyeol Ryu <dy@yoobato.com>
2017-08-02 01:58:56 +0800

    악... 인코딩 문제에서 벗어나야한다!!!

--- navergoodnewsparser.js
+++ navergoodnewsparser.js
@@ -1,19 +1,27 @@
 'use strict';
 
-var parser  = require('./lib/parser'),
-    log     = require('./lib/log');
+var parser      = require('./lib/parser'),
+    fs          = require('fs');
 
-log.info('*** 네이버 따뜻한 세상 뉴스 파서 시작 ***');
+console.log('*** 네이버 따뜻한 세상 뉴스 파서 시작 ***');
+
+if (fs.existsSync('./output.txt')) {
+    fs.unlinkSync('./output.txt');
+}
 
 var naverHost = 'news.naver.com';
 var parser = new parser(naverHost);
 
-var currentPage = 1;
-parser.parseNaverArticlePathList(currentPage, function(articlePaths) {
-    for (var i = 0; i < articlePaths.length; i++) {
-        var articlePath = articlePaths[i];
-        parser.parseNaverArticleMetadata(articlePath, function(articleOriginalUrl, articleDate, articleImageDesc) {
-            console.log("URL: " + articleOriginalUrl + " | Date: " + articleDate + " | Desc: " + articleImageDesc);
-        });
-    }
-});
+for (var page = 1; page <= 2; page++) {
+    parser.parseNaverArticlePathList(page, function(page, articlePaths) {
+        for (var i = 0; i < articlePaths.length; i++) {
+            var articlePath = articlePaths[i];
+            var count = 0;
+            parser.parseNaverArticleDetail(page, i, articlePath, function(page, index, articleOriginalUrl, articleTitle, articleDate, articleImageDesc) {
+
+                // TODO: 그냥... 또 request 보내자 후 ㅠ...
+                // fs.appendFileSync('./output.txt', articleOriginalUrl + '\t' + articleTitle + '\t' + articleDate + '\t' + articleImageDesc + '\n');
+            });
+        }
+    });
+}
